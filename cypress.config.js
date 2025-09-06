b0d9f01be64361abb0bd24e04a6f52ea1647cc2c
Sarithapr <sarithapr14@gmail.com>
2025-09-06 16:40:01 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'jztk3s',
+  projectId: 'jztk3s',
+   video: true,
+   screenshotOnRunFailure: true,
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
   e2e: {
     baseUrl: "https://www.saucedemo.com",
     reporter: 'mochawesome',
